202e09664c04ea5edb151ba5625a52b4d32a9485
Thiago Duarte <th7.duarte@gmail.com>
2024-02-28 19:59:41 -0300

    Exercícios Python

--- EX10.1 - Conversor Moeda v2.0.py
+++ EX10.1 - Conversor Moeda v2.0.py
@@ -1,3 +1,5 @@
+"""Este programa, faz calculo de quanto custará um produto na cotação atual."""
+
 print("="*77)
 print(" Olá Brasileiro, para saber o custo do produto/serviço estrangeiro, convertido em Real. \n Siga os passos a seguir:")
 print("="*77)
